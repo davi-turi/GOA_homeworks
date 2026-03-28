2faf771c3bdb794d07db69b5c2d549332669db80
davi-turi <ninodavituri45@gmail.com>
2026-03-28 10:43:09 +0400

    twenty-sixth commit

---
+++ GAMEOREBA/cw/n2.py
@@ -0,0 +1,5 @@
+string="lesson"
+i=0
+while i<len("lesson"):
+    print("lesson"[i])
+    i+=1
---
+++ GAMEOREBA/cw/n6.py
@@ -0,0 +1,4 @@
+list=["hello", "bonjur", "damaste", "merhaba", "ciao", "გამარჯობა", "hola"]
+
+list.pop(3)
+print(list)
