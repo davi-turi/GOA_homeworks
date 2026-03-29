7222c12c0a6c4f394a037896f0275f609e4165a5
davi-turi <ninodavituri45@gmail.com>
2026-03-29 20:07:00 +0400

    twenty-sixth commit

---
+++ Day 24.5/cw/n4.py
@@ -0,0 +1,5 @@
+list=["hello", "bonjur", "damaste", "merhaba", "ciao", "გამარჯობა", "hola"]
+
+string=input("Enter any string:")
+list.insert(4,string)
+print(list)
---
+++ Day 24.5/cw/n5.py
@@ -0,0 +1,3 @@
+nums=[21,22,23,24,25,26,27]
+nums.remove(24)
+print(nums)
